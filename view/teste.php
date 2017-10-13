b994341e96e939f6b709407a0a3a511a43a1ac25
Cleonilson <cleonilsonlopes@gmail.com>
2017-10-13 12:29:34 -0300

    Apos raty e angular

---
+++ view/teste.php
@@ -0,0 +1,27 @@
+<?php include_once("header.php");?>
+
+<!-- <div id="mais-buscados" class="container"> -->
+
+    <div class="row text-center title-default-roxo">
+
+                    <h2>os mais buscados</h2>
+                    <hr>    
+    </div>
+<!-- </div> -->
+
+
+                <div class="row">
+
+                    <div class="col-sm-3"></div>
+
+                    <div class="box-produto-info">
+                        <a href="#">
+                            <img src="img/produtos/panelas.png" alt="Panelas" class="produto-img">
+                            <h3>Conjunto de Panelas Tramontina Versalhes Alumínio Antiaderente 5</h3>
+                            <div class="estrelas"></div>
+                            <div class="text-qtd-reviews">(300)</div>
+                            <div class="text-valor text-roxo">R$ 109,90</div>
+                            <div class="text-parcelado text-arial-cinza">10 x de R$ 10,99 sem juros</div>
+                        </a>
+            </div>
+               </div>
